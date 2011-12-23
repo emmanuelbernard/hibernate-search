ebcf778ce6e16b209213d72c945b41f890fce128
Nicolas Helleringer <nicolas.helleringer@novacodex.net>
2011-12-23 16:09:08 +0100

    HSEARCH-923 Small indent fix

--- hibernate-search-engine/src/main/java/org/hibernate/search/engine/spi/AbstractDocumentBuilder.java
+++ hibernate-search-engine/src/main/java/org/hibernate/search/engine/spi/AbstractDocumentBuilder.java
@@ -670,9 +670,9 @@ public abstract class AbstractDocumentBuilder<T> {
 	private void bindSpatialAnnotation(String prefix, PropertiesMetadata propertiesMetadata, Spatial ann, XClass clazz, ConfigContext context) {
 		String fieldName = null;
 		if( !ann.name().isEmpty() ){
-			fieldName= prefix + ann.name();
+			fieldName = prefix + ann.name();
 		} else {
-			fieldName= clazz.getName();
+			fieldName = clazz.getName();
 		}
 		propertiesMetadata.classNames.add( fieldName );
 		propertiesMetadata.classStores.add( ann.store() );
